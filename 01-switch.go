10fb96ef3bdcea793e0949472f9f4263162e496a
fatmeme jadidi <fjadidi2001@gmail.com>
2022-07-25 12:37:19 -0400

    added gitignore & readme

--- 01-switch.go
+++ 01-switch.go
@@ -1,26 +1,26 @@
-package main
-
-import "fmt"
-
-func main(){
-	for i:=1;i<=1000{
-		switch i{
-		case i<=1000:
-			if  i%2==0:{
-
-			}
-
-			fmt.Println("This number is divisible by tow",i)
-		case
-			i%3==0:
-
-			fmt.Println("This number is divisible by three",i)
-		case
-			i%6==0:
-
-			fmt.Println("This number is divisible by six",i)
-
-		}
-	}
-
-}
+//package main
+//
+//import "fmt"
+//
+//func main(){
+//	for i:=1;i<=1000{
+//		switch i{
+//		case i<=1000:
+//			if  i%2==0:{
+//
+//			}
+//
+//			fmt.Println("This number is divisible by tow",i)
+//		case
+//			i%3==0:
+//
+//			fmt.Println("This number is divisible by three",i)
+//		case
+//			i%6==0:
+//
+//			fmt.Println("This number is divisible by six",i)
+//
+//		}
+//	}
+//
+//}
